d6c563c6f33ffa638ab4159052f3c05882ce23af
reyemtm <malcolmtmeyer@gmail.com>
2020-03-26 20:59:05 -0400

    switch to john hopkins data

--- covid19-oh/scrape-oh.js
+++ covid19-oh/scrape-oh.js
@@ -13,62 +13,63 @@ function formatDate(d) {
   return `${mo}/${da}/${ye}`
 }
 
+function formatDateDash(d) {
+  let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
+  let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d)
+  let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d)
+  return `${mo}-${da}-${ye}`
+}
+
 const dates = [
   formatDate(t), formatDate(yesterday), formatDate(twodays)
 ]
 
-let cache = JSON.parse(fs.readFileSync("./source/covid19-oh/covid19-oh-counties-timeseries.json"));
-fs.writeFileSync(`./source/covid19-oh/covid19-oh-counties-timeseries-cache-${dates[1].replace(/\//g, "-")}.json`, JSON.stringify(cache,0,2));
-
-fetch('https://coronavirus.ohio.gov/wps/portal/gov/covid-19/')
-.then(res => {
-  return res.text()
-})
-.then(data => {
-  let $ = cheerio.load(data);
-  let updated = $('.odh-ads__last-updated').text();
-  let siteDate = updated.split("\n")[2].trim() + "20";
-  let $cases = $('p:contains("Number of counties with cases:")').text();
-  let casesParsed = $cases.split(" – ")[1].trim();
-  // console.log(casesParsed)
-  let parsed = casesParsed.replace(/([()])/g, '');
-  let array = parsed.split(',');
-
-  let $d = $('p:contains("Number of deaths")').text();
-  console.log($d)
-  let d1 =  $d.split(" – ")[1].trim(); 
-  let dParsed = d1.replace(/([()])/g, '')
-  let dArray = dParsed.split(", ");
-
-  let dJoin = {};
-
-  dArray.map(d => {
-    dJoin[d.split(" ")[0]] = Number(d.split(" ")[1])
+fetch(`https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/${formatDateDash(t)}.csv`)
+  .then(res => res.text())
+  .then(csv => {
+    console.log("covid data download successful")
+    var results = Papa.parse(csv, {
+      header: true
+    });
+    processData(results.data)
   })
+  .catch(err => {
+    console.log(err)
+    process.exit(0)
+  });
+
+let cache = JSON.parse(fs.readFileSync("./source/covid19-oh/covid19-oh-counties-timeseries.json"));
+fs.writeFileSync(`./source/covid19-oh/covid19-oh-counties-timeseries-cache-${dates[1].replace(/\//g, "-")}.json`, JSON.stringify(cache, 0, 2));
+
+function processData(data) {
+  console.log("attempting to process covid data")
+
+  var countyData = data.filter(d => {
+    return d["Province_State"] === "Ohio"
+  });
 
   cache.map(c => {
     c.checked = new Date(Date.now());
-    c.dates[siteDate] = {};
-    c.dates[siteDate].cases = 0;
-    c.dates[siteDate].recovered = 0;
-    c.dates[siteDate].deaths = 0;
-        
-    for (let i in array) {
-      let county = array[i].trim();
-      let name = (county.split(" ")[0]).replace(" ", "");
-      if (c.name == name) {
-        c.dates[siteDate].cases = Number(county.split(" ")[1])
-        c.dates[siteDate].deaths = (!dJoin[name]) ? 0 : dJoin[name];
+    c.dates[dates[0]] = {};
+    c.dates[dates[0]].cases = 0;
+    c.dates[dates[0]].recovered = 0;
+    c.dates[dates[0]].deaths = 0;
+
+    for (let i in countyData) {
+      if (c.fips == countyData[i].FIPS) {
+        c.dates[dates[0]].cases = countyData[i].Confirmed
+        c.dates[dates[0]].deaths = countyData[i].Deaths;
+        c.dates[dates[0]].active = countyData[i].Active;
+        c.dates[dates[0]].recovered = countyData[i].Recovered
       }
     }
-
-    c.dates[siteDate].active = c.dates[siteDate].cases - c.dates[siteDate].deaths - c.dates[siteDate].recovered;
-    // c.new = null;
-    // console.log(dates[1])
-    // c.dailyrate = (!c.dates[siteDate].cases) ? 0 : (!c.dates[dates[1]].cases) ? 100 : c.dates[siteDate].cases / c.dates[dates[1]].cases
   })
 
-  fs.writeFileSync("./source/covid19-oh/covid19-oh-counties-timeseries.json", JSON.stringify(cache, 0,2))
+  // console.log(JSON.stringify(cache,0,2))
+
+  fs.writeFileSync("./source/covid19-oh/covid19-oh-counties-timeseries.json", JSON.stringify(cache, 0, 2))
+  
+  console.log("finished writing new covid data");
 
   var totalcases = [];
 
@@ -87,6 +88,78 @@ fetch('https://coronavirus.ohio.gov/wps/portal/gov/covid-19/')
 
   })
   fs.writeFileSync("./source/covid19-oh/covid19-oh-counties-cases.csv", Papa.unparse(totalcases))
+}
+
+//OLD SCRIPT FOR SCRAPING ODH DATA
+/*
+fetch('https://coronavirus.ohio.gov/wps/portal/gov/covid-19/')
+  .then(res => {
+    return res.text()
+  })
+  .then(data => {
+    let $ = cheerio.load(data);
+    let updated = $('.odh-ads__last-updated').text();
+    let siteDate = updated.split("\n")[2].trim() + "20";
+    let $cases = $('p:contains("Number of counties with cases:")').text();
+    let casesParsed = $cases.split(" – ")[1].trim();
+    // console.log(casesParsed)
+    let parsed = casesParsed.replace(/([()])/g, '');
+    let array = parsed.split(',');
+
+    let $d = $('p:contains("Number of deaths")').text();
+    console.log($d)
+    let d1 = $d.split(" – ")[1].trim();
+    let dParsed = d1.replace(/([()])/g, '')
+    let dArray = dParsed.split(", ");
+
+    let dJoin = {};
+
+    dArray.map(d => {
+      dJoin[d.split(" ")[0]] = Number(d.split(" ")[1])
+    })
+
+    cache.map(c => {
+      c.checked = new Date(Date.now());
+      c.dates[siteDate] = {};
+      c.dates[siteDate].cases = 0;
+      c.dates[siteDate].recovered = 0;
+      c.dates[siteDate].deaths = 0;
+
+      for (let i in array) {
+        let county = array[i].trim();
+        let name = (county.split(" ")[0]).replace(" ", "");
+        if (c.name == name) {
+          c.dates[siteDate].cases = Number(county.split(" ")[1])
+          c.dates[siteDate].deaths = (!dJoin[name]) ? 0 : dJoin[name];
+        }
+      }
+
+      c.dates[siteDate].active = c.dates[siteDate].cases - c.dates[siteDate].deaths - c.dates[siteDate].recovered;
+      // c.new = null;
+      // console.log(dates[1])
+      // c.dailyrate = (!c.dates[siteDate].cases) ? 0 : (!c.dates[dates[1]].cases) ? 100 : c.dates[siteDate].cases / c.dates[dates[1]].cases
+    })
+
+    fs.writeFileSync("./source/covid19-oh/covid19-oh-counties-timeseries.json", JSON.stringify(cache, 0, 2))
+
+    var totalcases = [];
+
+    cache.map(c => {
+      var cases = 0;
+      for (let d in c.dates) {
+        cases = c.dates[d].cases
+        c[d] = c.dates[d].cases
+      }
+      delete c.dates
+      delete c.rate
+      delete c.dailyrate
+      delete c.new
+      c.totalcases = cases
+      totalcases.push(c);
+
+    })
+    fs.writeFileSync("./source/covid19-oh/covid19-oh-counties-cases.csv", Papa.unparse(totalcases))
 
 
-});
+  });
+  */
